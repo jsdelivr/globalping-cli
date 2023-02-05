56f26f83046b4f1ef604f15f1a6208ddf6bde35f
Lotus <declininglotus@gmail.com>
2023-02-05 16:02:24 +0000

    fix: slice output to fit terminal when in live view

--- client/view.go
+++ client/view.go
@@ -6,8 +6,32 @@ import (
 	"time"
 
 	"github.com/charmbracelet/lipgloss"
-	"github.com/gosuri/uilive"
+	"github.com/pterm/pterm"
 )
+
+// Used to slice the output to fit the terminal in live view
+func sliceOutput(output string, w, h int) string {
+	// Split output into lines
+	lines := strings.Split(output, "\n")
+
+	// Subtract 2 lines from height to account for the header
+	h = h - 2
+
+	// If output is too long, slice it in reverse
+	if len(lines) > h {
+		lines = lines[len(lines)-h:]
+	}
+
+	// If any line is too long, slice it
+	for i, line := range lines {
+		if len(line) > w {
+			lines[i] = line[:w]
+		}
+	}
+
+	// Join lines back into a string
+	return strings.Join(lines, "\n")
+}
 
 func OutputResults(id string) {
 	// UI styles
@@ -15,10 +39,6 @@ func OutputResults(id string) {
 		Bold(true).Foreground(lipgloss.Color("#17D4A7"))
 
 	arrow := lipgloss.NewStyle().SetString(">").Bold(true).Foreground(lipgloss.Color("#17D4A7")).PaddingRight(1).String()
-
-	// Create new writer
-	writer := uilive.New()
-	writer.Start()
 
 	// Get results
 	data, err := GetAPI(id)
@@ -32,32 +52,49 @@ func OutputResults(id string) {
 		time.Sleep(100 * time.Millisecond)
 		data, err = GetAPI(id)
 		if err != nil {
-			fmt.Fprintf(writer, "%s", err)
-			writer.Stop()
+			fmt.Println(err)
 			return
 		}
 	}
+
+	// Create new writer
+	writer, _ := pterm.DefaultArea.Start()
+	w, h, _ := pterm.GetTerminalSize()
+
+	// String builder for output
+	var output strings.Builder
 
 	// Poll API every 100 milliseconds until the measurement is complete
 	for data.Status == "in-progress" {
 		time.Sleep(100 * time.Millisecond)
 		data, err = GetAPI(id)
+
+		// Reset string builder
+		output.Reset()
+
 		// Output every result in case of multiple probes
 		for _, result := range data.Results {
 			// Output slightly different format if state is available
 			if result.Probe.State != "" {
-				fmt.Fprintf(writer, arrow+highlight.Render("%s, %s, (%s), %s, ASN:%d")+"\n%s\n\n", result.Probe.Continent, result.Probe.Country, result.Probe.State, result.Probe.City, result.Probe.ASN, strings.TrimSpace(result.Result.RawOutput))
+				output.WriteString(arrow + highlight.Render(fmt.Sprintf("%s, %s, (%s), %s, ASN:%d", result.Probe.Continent, result.Probe.Country, result.Probe.State, result.Probe.City, result.Probe.ASN)))
 			} else {
-				fmt.Fprintf(writer, arrow+highlight.Render("%s, %s, %s, ASN:%d")+"\n%s\n\n", result.Probe.Continent, result.Probe.Country, result.Probe.City, result.Probe.ASN, strings.TrimSpace(result.Result.RawOutput))
+				output.WriteString(arrow + highlight.Render(fmt.Sprintf("%s, %s, %s, ASN:%d", result.Probe.Continent, result.Probe.Country, result.Probe.City, result.Probe.ASN)))
 			}
+
+			output.WriteString("\n" + strings.TrimSpace(result.Result.RawOutput) + "\n\n")
 		}
 
 		if err != nil {
-			fmt.Fprintf(writer, "%s", err)
 			writer.Stop()
+			fmt.Println(err)
 			return
 		}
+
+		writer.Update(sliceOutput(output.String(), w, h))
 	}
 
+	// Stop live updater and output to stdout
+	writer.RemoveWhenDone = true
 	writer.Stop()
+	fmt.Println(output.String())
 }
--- go.mod
+++ go.mod
@@ -4,25 +4,33 @@ go 1.19
 
 require (
 	github.com/charmbracelet/lipgloss v0.6.0
-	github.com/gosuri/uilive v0.0.4
+	github.com/pterm/pterm v0.12.54
 	github.com/spf13/cobra v1.6.1
 	github.com/stretchr/testify v1.8.1
 )
 
 require (
+	atomicgo.dev/cursor v0.1.1 // indirect
+	atomicgo.dev/keyboard v0.2.9 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gookit/color v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/pretty v0.3.0 // indirect
+	github.com/lithammer/fuzzysearch v1.1.5 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/muesli/reflow v0.2.1-0.20210115123740-9e1d0d53df68 // indirect
 	github.com/muesli/termenv v0.11.1-0.20220204035834-5ac8409525e0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
